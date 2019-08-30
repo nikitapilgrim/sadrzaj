8573c6f489515d498f85014062ed6b88cfe073b5
Nikita <nikita.pilgrim@icloud.com>
2019-08-30 13:48:34 +0300

    update

--- src/Data/Articles.js
+++ src/Data/Articles.js
@@ -14,6 +14,8 @@ export default [
           bukagije – moje belenzuke!”
           
           Lirska narodna pjesma (sevdalinka)`,
+    video: 'url',
+    audio: 'url',
     questions: [
       {
         title: 'This is title for 1st question',
@@ -40,11 +42,157 @@ export default [
           {
             title: 'događaji',
             right: false
+          },
+          {
+            title: 'i prema majci i prema mladiću',
+            right: false
           }
         ]
-      }
+      },
+      {
+        title: 'This is title for 3rd question',
+        question: 'O čemu majka misli i govori?',
+        answers: [
+          {
+            title: 'o „bjeljenju platna“, odnosno o poslu zbog koga je djevojka pošla',
+            right: false
+          },
+          {
+            title: 'o tome kako će izgrditi kćerku',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'This is title for 4th question',
+        question: 'O čemu kćerka misli i govori?',
+        answers: [
+          {
+            title: 'da ne može oprati rublje zbog mutne vode',
+            right: false
+          },
+          {
+            title: 'da se srela sa dragim i da posao nije ni započela',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'This is title for 5th question',
+        question: 'Razlike u mišljenju su izražene:',
+        answers: [
+          {
+            title: 'samo brojevima',
+            right: true
+          },
+          {
+            title: 'samo riječima',
+            right: false
+          },
+          {
+            title: 'i brojevima i riječima',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'This is title for 6th question',
+        question: '„…dragi mi je vodu zamutio!“ je:',
+        answers: [
+          {
+            title: 'izgovor što se djevojka predugo zadržala',
+            right: false
+          },
+          {
+            title: 'činjenica da je dragi konjem prešao preko vode i zamutio je pa zbog toga djevojka nije mogla „ubjeliti platno“.',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'This is title for 7th question',
+        question: 'Kroz djevojčino „odzivanje“ (odgovaranje majci) se naslućuje:',
+        answers: [
+          {
+            title: 'ljutnja',
+            right: false
+          },
+          {
+            title: 'radost zbog susreta sa dragim“.',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'This is title for 8th question',
+        question: 'Kćerka i majka su u:',
+        answers: [
+          {
+            title: 'bliskom odnosu',
+            right: true
+          },
+          {
+            title: 'u sukobu',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'This is title for 9th question',
+        question: 'Kletvom se izražava:',
+        answers: [
+          {
+            title: 'djevojčina želja da ostvari sreću sa svojim momkom',
+            right: true
+          },
+          {
+            title: 'djevojčina želja da se osveti momku',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'This is title for 10th question',
+        question: 'Pjesma nam dočarava…',
+        answers: [
+          {
+            title: 'djevojčinu tugu',
+            right: false
+          },
+          {
+            title: 'atmosferu sreće',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'This is title for 11th question',
+        question: 'Lirske narodne pjesme ispjevane su u:',
+        answers: [
+          {
+            title: 'lirskom osmercu',
+            right: false
+          },
+          {
+            title: 'lirskom desetercu',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'This is title for 12th question',
+        question: 'Sevdah je arapska riječ i znači:',
+        answers: [
+          {
+            title: 'praznik',
+            right: true
+          },
+          {
+            title: 'ljubav, ljubavna čežnja, ljubavni zanos',
+            right: false
+          }
+        ]
+      },
     ]
-
-
   }
 ]
