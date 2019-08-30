c0b654e48ee3bcf9556edcf711aadc240424a4fc
brrranko <info@branko-roric.com>
2019-08-30 17:50:40 +0200

    Added more articles

--- src/Data/Articles.js
+++ src/Data/Articles.js
@@ -194,7 +194,7 @@ export default [
         ]
       },
     ]
-  }
+  },
   {
     title: 'Narodna književnost',
     subtitle: 'Ljubavi rastanak',
@@ -297,6 +297,315 @@ Narodna lirska pjesma (sevdalinka)`,
           {
             title: 'epiteti',
             right: true
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Narodna književnost',
+    subtitle: 'Smrt Omera i Mejrime',
+    text: `Dvoje su se zamilili mladih:
+Omer momče, Mejrima djevojka,
+u proljeće kad im cvjeta cv’jeće,
+kad im cvjeta zumbul i karanfil.
+Upazi ih jedna mala straža,
+mala straža, Omerova majka,
+pa besjedi Omerova majka:
+“Mili sine, Omer’, momče mlado,
+ti ne ljubi Mejrime djevojke!
+Ljepšom će te oženiti majka –
+l’jepom Fatom novoga serdara.
+Još je Fata od roda bogata,
+i tebe će potpomoći blagom.”
+Аl’ besjedi Omer momče mlado:
+“Prođi me se, moja mila majko.
+Nije blago ni srebro ni zlato,
+već je blago što je srcu drago.”
+To ne sluša Omerova majka,
+već na silu oženila sina,
+i na silu dovela djevojku.
+Kad je bilo večer po večeri
+i mladence u ložnicu sveli,
+al’ besjedi Omer momče mlado:
+“Ao, Fato, ala ti si l’јера!
+Moja Mejra nije tako l’јера,
+al’ je Mejra srcu mome draga.
+Oj, boga ti, Fatima djevojko,
+donesi mi divit i hartije,
+da napišem do dv’je do tri r’ječi,
+jer je moja pobjedljiva majka,
+pa će reći da si m’ umorila.”
+Kad je majci knjigu napisao,
+on govori Fatimi djevojci:
+“Oj, boga ti, Fatima djevojko,
+kupajte me đulom rumenijem,
+prones’te me pokraj moje Mejre,
+nek me Mejra mrtvoga cjeliva
+kad me nije živog poljubila.
+Oj, boga ti, žalosna djevojko,
+ne pust’ glasa do bijela dana,
+nek se moja naveseli majka
+i sestrice kola naigraju
+i u kolu pjesme napjevaju.”
+To izusti pa i dušu pusti.
+Kad ujutru b’jeli dan osvanu,
+uranila Omerova majka,
+nosi kitu sitnoga bosiljka,
+da probudi dvoje mladenaca.
+Ciknu, viknu Fatima djevojka:
+“Oj, boga ti, mila moja majko,
+sinoć ti je Omer počinuo!”
+Аl’ besjedi Omerova majka:
+“Bog t’ ubio, Fatima djevojko!
+Ti si mi ga umorila mlada!”
+Аl’ besjedi Fatima djevojka:
+“Nisam, majko, života mi moga!
+Neg’ evo ti do dv’je do tri r’ječi
+što je tebi Omer ostavio.”
+Čita r’ječi Omerova majka,
+čita r’ječi pa suze proliva.
+Kupaše ga đulom rumenijem,
+pon’ješe ga Mejrinome dvoru.
+Аl’ besjedi Mejrima djevojka:
+“Đul miriše, mila moja majko,
+đul miriše oko našeg dvora;
+čini mi se – Omerova duša.”
+Аl’ besjedi l’јере Mejre majka:
+“Muč’, ne luduj, Mejrima djevojko!
+Muč’, ne luduj, kad budala nisi!
+Sad tvoj Omer drugu dragu ljubi,
+a za tebe mladu i ne mari!”
+Al’ besjedi Mejrima djevojka:
+“Đul miriše, mila moja majko,
+đul miriše – Omerova duša.”
+Ona strča dolje niz čardake,
+pa istrča na sokak na vrata,
+vidi granu sitnoga bisera,
+bogom brati do dva pobratima:
+“Čija li je grana od bisera?”
+Аl’ besjede do dva pobratima:
+“To je grana mladoga Omera.”
+Аl’ besjedi Mejrima djevojka:
+“Bogom braćo, do dva pobratima,
+spustite ga pred moje dvorove –
+da ga jadna mrtvoga cjelivam,
+kad ga nisam živa poljubila!”
+Spustiše ga pred Mejrine dvore,
+k njemu Mejra živa primaknula,
+mrtva Mejra crnoj zemlji pala.
+Sabljama joj sanduk satesaše.
+Kad Omera od dvora pon’ješe,
+tada Mejru u sanduk spustiše;
+kad Omera na groblje don’ješe,
+tada Mejru od dvora pon’ješe;
+kad Omera u raku spustiše,
+tada Mejru na groblje don’ješe;
+kad Omera zemljicom posuše,
+tada Mejru u raku spustiše.
+Tu se tuku do dv’je stare majke
+i proklinju i staro i mlado
+ko rastavi i milo i drago.
+
+Narodna balada`,
+    video: 'url',
+    audio: 'url',
+    questions: [
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Epskolirske pjesme govore...',
+        answers: [
+          {
+            title: 'uvijek o osjećanjima',
+            right: false
+          },
+          {
+            title: 'o nekom događaju, ali su prožete snažnim emocijama',
+            right: true
+          },
+          {
+            title: 'isključivo o bojevima i junacima',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Balade su…',
+        answers: [
+          {
+            title: 'izrazito tužne pjesme',
+            right: true
+          },
+          {
+            title: 'vesele, šaljive pjesme',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Stihovi „Nije blago ni srebro ni zlato, / već je blago što je srcu drago“, samostalno upotrijebljeni predstavljaju:',
+        answers: [
+          {
+            title: 'zagonetku',
+            right: false
+          },
+          {
+            title: 'poslovicu',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Aliteracija je…',
+        answers: [
+          {
+            title: 'uzastopno ponavljanje istog samoglasnika u nizu riječi',
+            right: false
+          },
+          {
+            title: 'uzastopno ponavljanje istog suglasnika u nizu riječi',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Pjesma se završava gradacijom. U njoj se osjećanja i slike nižu prema:',
+        answers: [
+          {
+            title: 'težini',
+            right: true
+          },
+          {
+            title: 'zanimljivosti',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Omer i Mejrima su simbol…',
+        answers: [
+          {
+            title: 'tragične ljubavi',
+            right: true
+          },
+          {
+            title: 'davno prošlog vremena',
+            right: false
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Narodna književnost',
+    subtitle: 'Tuturuza i šeh Meco (odlomak)',
+    text: `Tuturuza je ispričao najnoviju hridsku dogodovštinu. Slinić koji je kad je, ozgodnio i postao čaršijski prvak, promijenio prezime, oćutio je neku večer nekakvo orgecanje u magazi pod kućom. Pomislio je na hrsuza pa je uzdigao svu kućnu čeljad na noge da ga hvataju. I komšije su opkolile kuću i poredale se pred magazna vrata. Slinić je nagovarao hrsuza da izađe, da se ne boji, neće mu ništa biti. “Izađi slobodno! Neću te prijaviti policiji!” Štropot bakrenog suda nije prestajao, ali hrsuz nije htio napolje. Da ga ohrabri, Slinić ga je nagovarao da se ne stidi. “Slobodno ti izađi! I ja sam pod mladost bio takav!” Odškrinuo je magazna vrata. Najednom se nešto u magazi skrha, a na vrata jurne pas, krupan kao tele, i pobjegne između svijeta. Sve bi dobro bilo da nije bilo tuđeg svijeta, iz komšiluka. I tako se proču priča i sađe odmah ujutro do Baščaršije, pa Slinić ne može s mirom među svijet.
+Mahmutaga, dobričak i mehak čovjek, zacenuo se od smijeha. Sve ga suze oblile. Jedva je progovorio:
+– Bolji ti je dram smijeha, nego tovar suza.
+Tuturuza je malo promućkao u glavi, a onda hrabro progovorio, makar se malo i pribojavao da protuslovi uglednom čaršijskom starcu, posljednjem ćehaji abadžijskog esnafa:
+– I jedna suza može nekad vrijediti više od tovara zlata.
+– A oklen ti to znaš? – oglasio se prijekorno šeh Meco.
+Ako baš hoćete da znate, da vam ispričam.
+– Tamam! – pristade Mahmutaga.
+Ženskadija koja se spremala čuti neku pjesmu iznenadila se kad se Tuturuza podiže na koljena i poče, upravo kao kakav hodža sa ćursa, pričati priču koju niko od njih nije bio čuo.
+U Bagdadu, u glavnoj džamiji Azamiji, pred džumu bilo je puno svijeta, ali váiz nije došao. Svijet se osvrtao neće li ga opaziti kako ulazi na glavna vrata, da ustane ispred njega na noge, da se razmakne i napravi mu prolaz do ćursa. Ali, nije ga bilo. Bilo je još uleme, ali njega nije bilo, a nije svakom dano da vâzi. Je li bilo tako udešeno da on taj dan ne dođe pa da se zamoli učeni šejh Musaddik iz Kufe, koji je tih dana boravio u Bagdadu i upravo bio u Azamiji, da on taj dan vázi, jer je bio nadaleko znan kao učenjak i váiz, ne znam. Kako ne bî stalnog vaiza, nekoliko hodža zamoli šejha Musaddika da im on toga dana provazi. On se malo nećkao, ali ga zamoliše da im barem ispriča o čemu se u posljednje vrijeme priča u Kufi. Napokon pristade i pope se na ćurs.
+Svi su u nj uperili oči, a slijepi, i takvih na istoku ima uvijek dosta, pomalo su zakrenuli glavu ustranu da im uho na koje bolje čuju bude okrenuto prema ćursu.
+Šejh Musaddik se pomolio Bogu onom dovom kojom se Musa pejgamber obratio Bogu da mu rasprostrani prsa i odriješi čvor s jezika, a onda prešao na začudnu priču o događaju koji se nedavno zbio u Kufi, gdje se naširoko prepričavao.
+U Kufi je bio jedan trgovac vrlo bogat, koji je pošteno trgovao, lijepo zarađivao, ugledao sirotinju, davao robu i na veresiju, pozajmljivao i gotovinu bez kamata, a svijet mu se pošteno oduživao i nikad ga niko nije zakinuo, a ni on nikoga. Jednom trgovcu iz Basre dao je na veresiju dosta skupocjene robe i u tome je i umro, a da nije obavijestio ni sina koji je uza nj radio i naslijedio ga.
+I taj njegov sin je trgovao, radio, stanovao u očinskoj kući, imao ženu i djecu i dobro živio. Jedno vrijeme je trgovina napredovala, a on, kao otac mu, dijelio, kupovao, zajmao i posuđivao drugima, neki vraćali posuđeno i oduživali se, a neki ga, namjerno ili slučajno, zakidali pa se u dugu zemanu desilo da je spao na najtanje grane. Čak mu se trgovina potpuno ispraznila, kuća ogoljela, pa je došlo vrijeme da nije mogao sebe i svoju kućnu čeljad ni prehraniti.
+Smišljao šta da uradi pa odluči prodati kuću ne bi li se time neko vrijeme izdržavao, pa jeda jednom krene posao naprijed. Ali mu se nije dalo dati kuću na telala pa da se javno prodaje. Imao je jednog dobrog prijatelja pa ga poče tražiti da od njega uzajmi nešto malo novca dok ne proda kuću. Tražio ga je po svoj Kufi, a nigdje da ga susretne. Ni kući ga nije bilo. Gladan on, gladna mu kućna čeljad – mora ga naći pa gdje bilo da bilo. Pođe prema džamiji da klanja jer se primaklo vrijeme akšama, kad ugleda ahbaba kako nosi u ruci somun. Pozdraviše se, a trgovac ga odmah zamoli da mu pozajmi nešto novca dok ne proda kuću, a najprije je njemu nudi da je on kupi. Gladna su mu kućna čeljad, a i on, pa... Postiđen je oborio oči preda se, a prijatelj mu se popipa po džepovima, po pasu, ali kese s novcem nema.
+– Evo ti ovaj somun pa hajde kući, kad ste gladni, a ja ću ti poslije naći para koliko ti bude trebalo.
+Zahvali se onaj trgovac, uze somun pa se uputi odmah kući. Podigao onaj somun do usta da odgrize barem zalogaj, a hljeb miriše kao duša. Ne, neće ga načinjati nego će ga cijela donijeti kući.
+Tako misli trgovac i uputi se najbližim sokakom kući, kad niz sokak ide jedna žena, vodi dijete. Dijete kao švrače mršavo, izvalilo krupne crne oči pa njima jede onaj somun u trgovčevim rukama. Gurkā mater i pokazuje joj trgovčev somun. Kad dođoše uprema se žena zastade i zamoli trgovca da radi Allaha otkine samo zalogaj somuna jer joj je dijete gladno – i ne pamti kad su posljednji put jeli. Trgovac htjede otkinuti komad, ali kad ču da nisu odavno jeli, a on i njegova djeca su ipak nešto jučer pregrizli, dade ženi cio somun.
+Dijete razvuče lice od veselja, a materi potekoše dvije suze kao dva alema niz obraze.
+Pođe trgovac kući, ali od umora i gladi ne može dalje pa se spusti ukraj ulice i poče razmišljati:
+– Šta ja uradih? Kod kuće gladna moja djeca, gladna moja žena, ja gladan, a dadoh somun neznanoj ženi i njezinu djetetu. Jest da je sevab što sam nahranio gladnu ženu i gladno dijete, ali sam zgriješio prema svojoj porodici što sam ih ostavio da gladuju. Na jednoj strani sevab, a na drugoj grijeh. Ali nije to grijeh. I tu sam zaradio sevab, jer sam tuđina nahranio. To su tri sevaba. Dijete sam nahranio, mater mu nahranio, a treći je sevab što sam ono što je u onoj prilici bilo najpreče meni i mojoj čeljadi poklonio onima koji trpe kao i mi.
+Šejtan mu je došaptavao da to i nije sevab, da je grijeh što je uradio jer je zanemario one koji su mu najpreči, koji ovise o njemu, a on, trgovac, odgonio ga je u mislima od sebe i bio raspoložen što je dijete nahranio, što je svojim darom obradovao majku koja nije mogla uzdržati suze zahvalnice, a nikakav alem-kamen nije vrijedan kao iskrena suza.
+Mrak se počeo spuštati nad zemlju kad eto ti onog njegova prijatelja što mu je dao somun, sav se oznojio i zapuhao se od umora i trke. Ugleda trgovca pa će kao izvan sebe:
+– Gdje si, pobogu? Izgubih noge tražeći te... Svu sam Kufu obio, a tebe nigdje, kao da si u zemlju propao.
+– Šta je, što me tražiš? Jesi li mi našao kupca kući?
+– Kakav kupac, kakva kuća? Došao ti kući trgovac koji ti se zadužio kod oca, pa eno sad na tri deve donio blago, sav dug i hedije. Nije ni znao da ti je otac umro, pa hajde sad ti primi to imáće.
+
+Alija Nametak`,
+    video: 'url',
+    audio: 'url',
+    questions: [
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Poenta prvog dijela priče je:',
+        answers: [
+          {
+            title: 'Bolji ti je dram smijeha, nego tovar suza.',
+            right: true
+          },
+          {
+            title: 'Bolji ti je dram smijeha, nego tovar blaga.',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Drugi dio priče najavljuje mudrost:',
+        answers: [
+          {
+            title: 'Jedna suza može vrijediti više od tovara smijeha.',
+            right: false
+          },
+          {
+            title: 'I jedna suza može nekad vrijediti više od tovara zlata.',
+            right: true
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'O čijoj je suzi ovdje riječ?',
+        answers: [
+          {
+            title: 'trgovčevoj',
+            right: false
+          },
+          {
+            title: 'majčinoj',
+            right: true
+          },
+          {
+            title: 'djetetovoj',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Koja bi poslovica odgovarala trgovčevom postupku (dao je somun sirotici iako je u njegovoj kući nestalo hrane):',
+        answers: [
+          {
+            title: 'Dobro se dobrim vraća.',
+            right: true
+          },
+          {
+            title: 'Dobar i lud su braća.',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'Odaberi tačan odgovor',
+        question: 'Pričalac je ispričao priču da bi one koji su je slušali…',
+        answers: [
+          {
+            title: 'poučio dobroti',
+            right: true
+          },
+          {
+            title: 'zabavio',
+            right: false
           }
         ]
       }
