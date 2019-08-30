61648dc651dd19da1bd67156eb6086234affb204
Nikita <nikita.pilgrim@icloud.com>
2019-08-30 13:25:18 +0300

    update

---
+++ src/Data/Articles.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: 'Narodna književnost',
+    subtitle: 'Majka Fatu kroz tri gore viče',
+    text: `Majka Fatu kroz tri gore viče:
+          “Jesi l’, kćeri, ub’jelila platno?”
+          Fata joj se kroz sedam odziva:
+          “Nisam, majko, ni do vode došla,
+          dragi mi je vodu zamutio!
+          Kun’ ga, majko, i ja ću ga kleti,
+          samo stani, ja ću započeti:
+          tamnica mu moja njedra bila,
+          sindžir-halke – moje b’jele ruke,
+          bukagije – moje belenzuke!”
+          
+          Lirska narodna pjesma (sevdalinka)`,
+    questions: [
+      {
+        title: 'This is title for 1st question',
+        question: 'Lirski svijet ove pjesme čine:',
+        answers: [
+          {
+            title: 'osjećanja',
+            right: true
+          },
+          {
+            title: 'događaji',
+            right: false
+          }
+        ]
+      },
+      {
+        title: 'This is title for 2nd question',
+        question: 'Prema kome djevojka ispoljava osjećanja:',
+        answers: [
+          {
+            title: 'samo prema majci',
+            right: true
+          },
+          {
+            title: 'događaji',
+            right: false
+          }
+        ]
+      }
+    ]
+
+
+  }
+]
